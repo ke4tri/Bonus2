52ee47b665f9a4bfe0334b23f80b79ae45ccc06e
Wayne Collier <ke4tri@gmail.com>
2018-10-07 18:40:34 -0500

    click on and click off both have an add to array of prices and a remove from princes array. still have to iterate over array on removal and make sure the correct price is being removed and not just the pop() (last item added)

--- Javascripts/Components/bread.js
+++ Javascripts/Components/bread.js
@@ -1,18 +1,48 @@
 import {add} from '../Helpers/util.js';
 import {choices, totalCost} from '../Components/sandComponents.js'
 
-
 const sendToAdd = (amount) => {
     totalCost.push(amount);
-    console.log("check totalCost array");
+    console.log(totalCost);
 };
 
-const button1 = document.querySelector("input[name=customRadio1]");
-button1.addEventListener("click", function() {
-        let itemcost = `${choices[0].white}`
-        console.log("This is the prices " + itemcost )
-        sendToAdd(itemcost);
-  
-});
+const sendToRem = (amount) => {
+    totalCost.pop(amount);
+    console.log(totalCost);
+};
 
+let button1 = document.querySelector("input[name=customRadio1]");
+let setCheck1;
 
+    button1.onclick = function(){
+        if(setCheck1 != this){
+             setCheck1 = this;
+             let itemcost = `${choices[0].white}`
+             console.log("This is the prices " + itemcost )
+             sendToAdd(itemcost);
+        }else{
+            this.checked = false;
+            setCheck1 = null;
+            let itemcost = `${choices[0].white}`
+            sendToRem(itemcost);
+            console.log("This is total price " + itemcost)
+    };
+}
+
+let button2 = document.querySelector("input[name=customRadio2]");
+let setCheck2;
+
+    button2.onclick = function(){
+        if(setCheck2 != this){
+             setCheck2 = this;
+             let itemcost = `${choices[0].wheat}`
+             console.log("This is the prices " + itemcost )
+             sendToAdd(itemcost);
+        }else{
+            this.checked = false;
+            setCheck2 = null;
+            let itemcost = `${choices[0].wh}`
+            sendToRem(itemcost);
+            console.log("This is total price " + itemcost)
+    };
+}
